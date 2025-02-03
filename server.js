b959d3d070cc404709f16eb5e79af8f5c45bf8c6
lovegupta112 <loveguptaofficials@gmail.com>
2025-02-03 15:02:28 +0530

    added initial setup github actions for chatapp

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ io.on("connection", (socket) => {
 });
 
 //in this port , our server will run
-const PORT = 9003;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   // console.log('happy coding....');
